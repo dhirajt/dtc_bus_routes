afbe0136636ae02f4e058f75f7f50f3bd647345d
Dhiraj Thakur <dhirajk.92@gmail.com>
2013-02-22 00:06:26 +0530

    Basic Skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+dtc-bus-routes
+==============
+
+This project is inspired from http://busroutes.in and of course <br />
+the official site of DTC http://delhigovt.nic.in/dtcbusroute/dtc/Find_Route/getroute.asp !
+
+
+UNDER CONSTRUCTION!
+===================
+
---
+++ dtcbusroutes/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+from django.views.generic.simple import direct_to_template
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', direct_to_template, {'template' : 'index.html' }, name='home'),
+    # url(r'^dtcbusroutes/', include('dtcbusroutes.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
