f6b9ab1100139d958bc6af50ca43d71338d90302
Dhiraj Thakur <dhirajk.92@gmail.com>
2013-02-23 00:24:20 +0530

    Added DB(mysql) support

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 dtc-bus-routes
 ==============
 
+A web app to help you commute in Delhi!
+
 This project is inspired from http://busroutes.in and of course <br />
 the official site of DTC http://delhigovt.nic.in/dtcbusroute/dtc/Find_Route/getroute.asp !
 
--- dtcbusroutes/settings.py
+++ dtcbusroutes/settings.py
@@ -14,12 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'busroutes_schema', 
+        'USER': 'root',                     
+        'PASSWORD': 'sql123',                  
+        'HOST': '',                      
+        'PORT': '',                 
     }
 }
 
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
 
-   # 'south', 
+    'south', 
 
 )    
 # A sample logging configuration. The only tangible logging
