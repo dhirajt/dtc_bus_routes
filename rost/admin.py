350552074841f7715693bc8ddc9d987e9f3ed4a0
Dhiraj Thakur <dhirajk.92@gmail.com>
2013-04-26 10:27:41 +0530

    Autocomplete working

--- rost/admin.py
+++ rost/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import Stage, Route
+from models import Stage, Route, StageSeq
 
 admin.site.register(Route)
 admin.site.register(Stage)
-
+admin.site.register(StageSeq)
--- rost/models.py
+++ rost/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Stage(models.Model):
 	name = models.CharField(max_length=64)
 	lattitude = models.FloatField(null=True, blank=True)
@@ -13,7 +14,15 @@ class Route(models.Model):
 	name = models.CharField(max_length=64)
 	start = models.ForeignKey(Stage, related_name='start')
 	end = models.ForeignKey(Stage, related_name='end')
-	stages = models.ManyToManyField(Stage)
+	stages = models.ManyToManyField(Stage,through="StageSeq")
 	
 	def __unicode__(self):
 	    return self.name
+	    
+class StageSeq(models.Model):
+    route = models.ForeignKey(Route)
+    stage = models.ForeignKey(Stage)
+    sequence = models.IntegerField()
+    
+    def __unicode__(self):
+        return str(self.route.id)+' | '+str(self.stage.id)+'('+str(self.sequence)+')'
