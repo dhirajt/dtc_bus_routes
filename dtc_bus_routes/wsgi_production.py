a9c572af780fb4c1a8814d24869620468739f231
Dhiraj Thakur <dhirajk.92@gmail.com>
2015-07-13 00:33:47 +0530

    updated ssl redirect and newrelic setup

--- dtc_bus_routes/wsgi_production.py
+++ dtc_bus_routes/wsgi_production.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 import newrelic.agent
 
-newrelic.agent.initialize('newrelic.ini')
+newrelic.agent.initialize()
 
 from django.core.wsgi import get_wsgi_application
 
