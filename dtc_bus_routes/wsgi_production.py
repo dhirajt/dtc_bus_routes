85f91f0ef1fb487a26b78ea46490d4dde032f133
Dhiraj Thakur <dhirajk.92@gmail.com>
2015-07-12 23:03:36 +0530

    updated new relic monitoring

--- dtc_bus_routes/wsgi_production.py
+++ dtc_bus_routes/wsgi_production.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+
+newrelic.agent.initialize('newrelic.ini')
 
 from django.core.wsgi import get_wsgi_application
 
