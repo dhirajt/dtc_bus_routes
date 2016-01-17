2c053a6ac82ad3aff59bb8aaf8427c31c49c23d5
Dhiraj Thakur <dhirajk.92@gmail.com>
2016-01-17 21:46:41 +0530

    added indirect routes :rocket: don't know why it took so long :disappointed:

--- busroutes/views.py
+++ busroutes/views.py
@@ -3,6 +3,7 @@ import json
 import redis
 
 from django.http import HttpResponse, Http404, HttpResponseBadRequest
+from django.db.models import Prefetch
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
@@ -12,7 +13,6 @@ from models import Stage, Route, StageSequence
 
 def server_error(request):
     return render(request,template_name="500.html")
-
 
 def search_by_num(request):
     if request.GET:
@@ -99,47 +99,51 @@ def ajax_stage_search(request):
                 rclient.expire(query,6*60*60)
     return HttpResponse("\n".join(stages))
 
-def bus_by_stages(request,source='',destination=''):
-    buses = Route.objects.filter(
+def bus_by_stage(request,source='',destination=''):
+    direct_buses = Route.objects.filter(
             is_active=True,
             stages__name_slug=source).filter(stages__name_slug=destination)
 
-    if buses.exists():
-        startstage = Stage.objects.get(name_slug=source)
-        endstage =  Stage.objects.get(name_slug=destination)
+    stages = list(Stage.objects.filter(name_slug__in=[source,destination]))
+    startstage,endstage = stages[0],stages[1]
 
-        data_payload = payloadmaker(buses, startstage, endstage)
-        payload = {
+    if direct_buses.exists():
+        payload = payloadmaker(direct_buses, startstage, endstage)
+        context = {
+            'direct_routes':payload,
             'startstage': startstage.name,
-            'endstage': endstage.name,
-            'data': data_payload,
+            'endstage': endstage.name
         }
-        return render(request, "results_by_stage.html", payload)
     else:
-        return redirect('search_by_stage')
+        payload = indirect_route_finder(startstage,endstage)
+        context = {
+            'indirect_routes':payload,
+            'startstage': startstage.name,
+            'endstage': endstage.name
+        }
+    if not payload:
+        url = reverse('search_by_stage') + '?redirect_error=true'
+        return redirect(url)
+    return render(request, "results_by_stage.html", context)
 
 def search_by_stage(request):
     if request.GET:
-        startstage = request.GET['startstage']
-        endstage = request.GET['endstage']
-        buses = Route.objects.filter(
-            is_active=True,stages__name=startstage).filter(stages__name=endstage)
+        redirect_error = request.GET.get('redirect_error',None)
+        startstage = request.GET.get('startstage','')
+        endstage = request.GET.get('endstage','')
 
-        buses_exist = buses.exists()
         source = list(Stage.objects.filter(name=startstage))
         destination =  list(Stage.objects.filter(name=endstage))
 
-        if buses_exist and source and destination:
+        if source and destination and not redirect_error:
             return redirect('bus_by_stage',source=source[0].name_slug,
                 destination=destination[0].name_slug)
         else:
-            error = 'Either you entered wrong stop name or no direct route \
-                 exists!'
+            error = 'Either you entered wrong stop name or no route exists!'
             return render(request, "search_by_stage.html", {"error": error})
 
     else:
         return render(request, "search_by_stage.html")
-
 
 def payloadmaker(buses, startstage, endstage):
     data = {}
@@ -169,3 +173,62 @@ def payloadmaker(buses, startstage, endstage):
     buslist = [shortest] + [ i for i in data.keys() if i!=shortest ]
     payload = [(bus,data[bus]) for bus in buslist]
     return payload
+
+def indirect_route_finder(startstage=None,endstage=None):
+	if not (startstage and endstage):
+		return []
+
+	routes_start = list(Route.objects.filter(stages=startstage,is_active=True).prefetch_related(
+		Prefetch('stages',queryset=Stage.objects.all().order_by('stagesequence__sequence'))).all())
+	routes_end = list(Route.objects.filter(stages=endstage,is_active=True).prefetch_related(
+		Prefetch('stages',queryset=Stage.objects.all().order_by('stagesequence__sequence'))).all())
+
+	route_list = []
+
+	for rs in routes_start:
+		for re in routes_end:
+			stages_rs = list(rs.stages.all())
+			stages_re = list(re.stages.all())
+
+			changeovers = list(set(stages_rs) & set(stages_re))
+
+			if changeovers:
+				index_startstage = stages_rs.index(startstage)
+				index_endstage = stages_re.index(endstage)
+
+				potential_changeovers = [
+					(stages_rs.index(item),abs(index_startstage - stages_rs.index(item))) for item in changeovers]
+
+				potential_changeovers = sorted(potential_changeovers,key=lambda item:item[1])
+
+				first_changeover_index_rs = potential_changeovers[0][0]
+				first_changeover = stages_rs[first_changeover_index_rs]
+
+				start_stages = []
+				if first_changeover_index_rs > index_startstage:
+					start_stages = stages_rs[index_startstage:first_changeover_index_rs+1]
+				else:
+					start_stages = stages_rs[first_changeover_index_rs:index_startstage+1]
+					start_stages.reverse()
+
+				end_stages = []
+				first_changeover_index_re = stages_re.index(first_changeover)
+				if first_changeover_index_re > index_endstage:
+					end_stages = stages_re[index_endstage:first_changeover_index_re+1]
+					end_stages.reverse()
+				else:
+					end_stages = stages_re[first_changeover_index_re:index_endstage+1]
+
+				indirect_route = {
+					'start_stages':{
+						'route': rs,
+						'stages': start_stages
+					},
+					'end_stages': {
+						'route': re,
+						'stages': end_stages
+					},
+					'changeover':first_changeover
+				}
+				route_list.append(indirect_route)
+	return route_list
