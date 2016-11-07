6763eb5383e05adeeb59aa278ecde9fc41825021
Dhiraj Thakur <dhirajk.92@gmail.com>
2016-11-07 09:44:34 +0530

    added some more apis

--- api/views.py
+++ api/views.py
@@ -17,6 +17,41 @@ from busroutes.models import Stage, Route
 from .responses import BusRoutesStandardResponse
 
 
+aescrypt = AesCrypt256()
+KEY = os.getenv('ENDPOINT_KEY','')
+
+def get_final_endpoint(endpoint_name='',payload=''):
+    BASE_ENDPOINT = os.getenv('BASE_ENDPOINT','')
+    endpoint_dict = {
+        'stage_eta' : os.getenv('STAGE_ETA_ENDPOINT',''),
+        'route_eta' : os.getenv('ROUTE_ETA_ENDPOINT','')
+    }
+    aid = uuid.uuid4().hex[:16]
+    endpoint_path = endpoint_dict.get(endpoint_name,'')
+
+    url = BASE_ENDPOINT + aescrypt.encryptB64URLSafe(
+        KEY, ';'.join([endpoint_path+aid,payload]))
+
+    rclient = redis.StrictRedis(connection_pool=settings.API_CACHE_REDIS_POOL)
+    proxies = {}
+    proxy_list = rclient.lrange('proxies', 0, -1)
+    if proxy_list:
+        if 'http' in proxy_list[0]:
+            proxies['http'] = proxy_list[0]
+        elif 'https' in proxy_list[0]:
+            proxies['https'] = proxy_list[0]
+
+    return url, proxies
+
+def get_endpoint_headers():
+    headers = {
+        'User-Agent': os.getenv('ENDPOINT_UA',''),
+        'Host': os.getenv('ENDPOINT_HOST',''),
+        'Connection': os.getenv('ENDPOINT_CONNECTION',''),
+        'Accept-Encoding': os.getenv('ENDPOINT_AE',''),
+    }
+    return headers
+
 def get_paginated_response(queryset,request,serailizer):
     pagination_class = getattr(api_settings,'DEFAULT_PAGINATION_CLASS',None)
     if not pagination_class:
@@ -149,6 +184,200 @@ def route_search(request):
         'results':serializer.data
     }
     return BusRoutesStandardResponse(response_data)
+
+@api_view(['GET'])
+def stage_eta(request):
+    """
+    Get eta of buses on a stage.
+    """
+    stage_id = request.GET.get('stage_id','')
+    if not stage_id:
+        raise ParseError()
+
+    if stage_id.isdigit():
+        stages = list(Stage.objects.filter(
+            Q(uid=stage_id) | Q(id=stage_id)))
+    else:
+        stages = list(Stage.objects.filter(uid=stage_id))
+
+    if not stages:
+        raise NotFound()
+
+    stage_id = stages[0].uid
+
+    eta_list = []
+
+    rclient = redis.StrictRedis(connection_pool=settings.API_CACHE_REDIS_POOL)
+    cached_eta_list = rclient.get('stage_eta:'+stage_id)
+    if cached_eta_list:
+        eta_list = json.loads(cached_eta_list)
+    else:
+        url, proxies = get_final_endpoint(
+            endpoint_name='stage_eta',payload=stage_id)
+        headers = get_endpoint_headers()
+
+        response = requests.get(url, proxies=proxies)
+
+        if response.ok:
+            response_json_text = aescrypt.decryptB64(KEY,response.text)
+            try:
+                response_json = json.loads(response_json_text)
+            except ValueError:
+                response_json = ''
+
+            if response_json and response_json['etaList']:
+                for item in response_json['etaList']:
+                    seat_availability = 'Unknown'
+                    if 'available' in item['occupancy'].lower():
+                        seat_availability = 'Available'
+
+                    bus_type = 'DTC'
+                    if item['typeOfBuses'] == '8':
+                        bus_type = 'Cluster'
+
+                    passengers = None
+                    if item['passengers'].isdigit():
+                        passengers = int(item['passengers'])
+
+                    latitude = None
+                    longitude = None
+
+                    eta_list.append({
+                        'passengers': passengers,
+                        'destination': item['destination'],
+                        'route_id': item['routeId'],
+                        'eta_minutes': int(item['eta']),
+                        'location': item['location'],
+                        'bus_number': item['busno'],
+                        'seat_availability':seat_availability,
+                        'bus_type': bus_type,
+                        'latitude': latitude,
+                        'longitude': longitude
+                    })
+    rclient.setex('stage_eta:'+stage_id, 30, json.dumps(eta_list))
+    eta_serializer = StageETAListSerializer(
+        eta_list,
+        many=True,
+        context={'request': request})
+
+    serializer = StageETASerializer(
+        stages,
+        many=True,
+        context={'request': request, 'eta_list':eta_serializer})
+
+    return BusRoutesStandardResponse(serializer.data)
+
+
+@api_view(['GET'])
+def route_eta(request):
+    """
+    Get eta of buses on a route.
+    """
+    route_id = request.GET.get('route_id','')
+    stage_id = request.GET.get('stage_id','')
+
+    if not route_id:
+        raise ParseError()
+
+    if route_id.isdigit():
+        routes = list(Route.objects.filter(
+            Q(uid=route_id) | Q(id=route_id)))
+    else:
+        routes = list(Route.objects.filter(uid=route_id))
+
+    if not routes:
+        raise NotFound()
+
+    route = routes[0]
+    route_id = route.uid
+
+    eta_list = []
+
+    rclient = redis.StrictRedis(connection_pool=settings.API_CACHE_REDIS_POOL)
+    cached_data = rclient.get('route_eta:'+route_id)
+
+    data = {}
+    if cached_data:
+        data = json.loads(cached_data)['eta_list']
+    else:
+        url, proxies = get_final_endpoint(
+            endpoint_name='route_eta',payload=route_id)
+        headers = get_endpoint_headers()
+
+        response = requests.get(url, proxies=proxies)
+
+        if response.ok:
+            response_json_text = aescrypt.decryptB64(KEY,response.text)
+            try:
+                response_json = json.loads(response_json_text)
+            except ValueError:
+                response_json = ''
+
+            vehicle_list = {}
+            eta_list = []
+
+            if response_json and response_json['vehicleList']:
+                for item in response_json['vehicleList']:
+                    if item['vehicleNo']:
+                        vehicle_number, extra_text = item['vehicleNo'].strip().split(' ',1)
+
+                        location, seat_availability = extra_text.split(
+                            '\n')[1].split(', ')
+
+                        if 'available' in seat_availability:
+                            seat_availability = 'Available'
+
+                        vehicle_list[vehicle_number] = {
+                            'location': location,
+                            'latitude': item['latitude'],
+                            'longitude': item['longitude'],
+                            'seat_availability': seat_availability
+                        }
+
+            if response_json and response_json['allBusStops']:
+                for item in response_json['allBusStops']:
+                    seat_availability = 'Unknown'
+                    if item['occupancy'] and 'available' in item['occupancy'].lower():
+                        seat_availability = 'Available'
+
+                    passengers = None
+                    if item['passengers'] and item['passengers'].isdigit():
+                        passengers = int(item['passengers'])
+
+                    latitude = item['latitude']
+                    longitude = item['longitude']
+
+                    eta_minutes = []
+
+                    name_split = item['Name'].strip().split('\n',1)
+                    if len(name_split) > 1:
+                        eta_minutes = map(
+                            int,re.findall('(\d+)',name_split[1]))
+
+                    eta_list.append({
+                        'passengers': passengers,
+                        'eta_minutes': eta_minutes,
+                        'seat_availability':seat_availability,
+                        'latitude': latitude,
+                        'longitude': longitude
+                    })
+            data['vehicle_list'] = vehicle_list
+            data['eta_list'] = eta_list
+
+    if any(data.values()):
+        rclient.setex('route_eta:'+route_id, 30, json.dumps(data))
+
+    eta_serializer = RouteStageETAListSerializer(
+        eta_list,
+        many=True,
+        context={'request': request})
+
+    serializer = StageETASerializer(
+        stages,
+        many=True,
+        context={'request': request, 'eta_list':eta_serializer})
+
+    return BusRoutesStandardResponse(serializer.data)
 
 @api_view(('GET',))
 @permission_classes((AllowAny, ))
